9beccd006e7eff745cf2b2d8dbdc189ab45853b4
6dy <287494136@qq.com>
2019-04-19 14:56:38 +0800

    second

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -3,20 +3,22 @@ const path = require('path')
 const utils = require('./utils')
 const config = require('../config')
 const vueLoaderConfig = require('./vue-loader.conf')
+const { VueLoaderPlugin } = require("vue-loader")
+
 
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
 }
 
 const createLintingRule = () => ({
-  test: /\.(js|vue)$/,
-  loader: 'eslint-loader',
-  enforce: 'pre',
-  include: [resolve('src'), resolve('test')],
-  options: {
-    formatter: require('eslint-friendly-formatter'),
-    emitWarning: !config.dev.showEslintErrorsInOverlay
-  }
+  // test: /\.(js|vue)$/,
+  // loader: 'eslint-loader',
+  // enforce: 'pre',
+  // include: [resolve('src'), resolve('test')],
+  // options: {
+  //   formatter: require('eslint-friendly-formatter'),
+  //   emitWarning: !config.dev.showEslintErrorsInOverlay
+  // }
 })
 
 module.exports = {
@@ -31,6 +33,10 @@ module.exports = {
       ? config.build.assetsPublicPath
       : config.dev.assetsPublicPath
   },
+  plugins: [
+    // make sure to include the plugin for the magic
+    new VueLoaderPlugin()
+  ], 
   resolve: {
     extensions: ['.js', '.vue', '.json'],
     alias: {
@@ -43,9 +49,14 @@ module.exports = {
       ...(config.dev.useEslint ? [createLintingRule()] : []),
       {
         test: /\.vue$/,
-        loader: 'vue-loader',
-        options: vueLoaderConfig
-      },
+        loader: "vue-loader",
+        options: {
+            loaders: {
+                scss: "vue-style-loader!css-loader!sass-loader",
+                sass: "vue-style-loader!css-loader!sass-loader?indentedSyntax"
+            }
+        }
+    },
       {
         test: /\.js$/,
         loader: 'babel-loader',
